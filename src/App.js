a1905f4be9aeb95266f6c7c331cc165ab4bf2598
amrutha <amrtha1994@gmail.com>
2023-06-29 19:06:09 +0530

    Addmovietrailor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import React from 'react';
+ import {comedyMovies,action,orginals ,HorrorMovies,Documentaries} from './urls'
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar/>
+    <Banner/>
+    <RowPost url={orginals} title="Netflix Orginals"/>
+    <RowPost url={action} title="Action" isSmall/>
+    <RowPost url={comedyMovies} title="comedy" isSmall/>
+    <RowPost url={HorrorMovies} title="Horror" isSmall/>
+    <RowPost url={Documentaries} title="Documentaries" isSmall/>
+    
+    
+   
     </div>
   );
 }
